ad8fed4a14f29f97af14c5b71aa408018f898db2
Dillon Christensen <j.dillon.christensen@gmail.com>
2021-09-11 00:06:37 -0500

    tidied up a bit and found formula name

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,14 +1,5 @@
 import nonAdverbs from "./non-adverbs.js";
 
-export function* getSentences(excerpt) {
-  for (const line of excerpt.split("\n")) {
-    const clean = line.trim();
-    if (clean) {
-      yield getSentencesFromLine(clean);
-    }
-  }
-}
-
 export function getSentencesFromLine(line) {
   return line
     .split(/(?<!\bmr?s?|\bMr?s?|\b[dDjJsS]r)[.!?]/)
@@ -18,21 +9,19 @@ export function getSentencesFromLine(line) {
 
 export const getWords = sentence => sentence.split(" ").filter(Boolean);
 
-export const getSyllables = word => {
+export function getSyllables(word) {
   const matches = word
     .replace(/^y/i, "")
     .replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, "")
     .match(/[aeiouy]{1,2}/g);
   return matches ? matches.length : 1;
-};
-
-export const getAdverbs = words => words.filter(isAdverb).length;
+}
 
 export function isAdverb(word) {
   return /ly\b/i.test(word) && !nonAdverbs.some(re => re.test(word));
 }
 
-export default excerpt => {
+export default function (excerpt) {
   if (!excerpt || !excerpt.length) {
     return {};
   }
@@ -57,20 +46,21 @@ export default excerpt => {
     }
   }
 
-  const adverbPercentage =
-    ((totals.adverbs / totals.words) * 100).toFixed(2) + "%";
+  /*
+   * forumla comes from:
+   * https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests#Flesch%E2%80%93Kincaid_grade_level
+   */
   const grade = (
     0.39 * (totals.words / totals.sentences) +
     11.8 * (totals.syllables / totals.words) -
     15.59
   ).toFixed(2);
+  const adverbPercentage =
+    ((totals.adverbs / totals.words) * 100).toFixed(2) + "%";
 
   return {
-    sentences: totals.sentences,
-    words: totals.words,
-    adverbs: totals.adverbs,
+    ...totals,
     adverbPercentage,
-    syllables: totals.syllables,
     grade
   };
-};
+}
